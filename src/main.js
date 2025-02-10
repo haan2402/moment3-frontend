016edcda01f344e8beeb644b6e037b4b4a870668
Hanna Angeria <haan2402@student.miun.se>
2025-02-10 14:26:37 +0100

    lagt till menu och base för scss och gjort några variabler som jag testat infoga i main scss

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+//element till navigeringsmenyn i mobilläge
+let openButton = document.getElementById("open-menu");
+let closeButton = document.getElementById("close-menu");
+let navEl = document.getElementById("nav-menu");
+
+//skapar två eventlyssnare för öppna och stänga knappen i hambugermenyn
+openButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
+
+//skapar en funktion för att sedan toggla mellan öppna och stänga i hamburger menyn
+function toggleMenu() {
+    navEl.classList.toggle("active");
+}
