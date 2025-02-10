c949bd8fe054a5039c63415f2525ab95ef525aa7
Hanna Angeria <haan2402@student.miun.se>
2025-02-10 15:18:25 +0100

    styling för header och navigering, använt olika partials och variabler

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 //element till navigeringsmenyn i mobilläge
 let openButton = document.getElementById("open-menu");
 let closeButton = document.getElementById("close-menu");
-let navEl = document.getElementById("nav-menu");
 
 //skapar två eventlyssnare för öppna och stänga knappen i hambugermenyn
 openButton.addEventListener('click', toggleMenu);
@@ -11,5 +10,14 @@ closeButton.addEventListener('click', toggleMenu);
 
 //skapar en funktion för att sedan toggla mellan öppna och stänga i hamburger menyn
 function toggleMenu() {
-    navEl.classList.toggle("active");
+    let navEl = document.getElementById("nav-menu");
+
+    //hämtar styling
+    let style = window.getComputedStyle(navEl);
+
+    if(style.display === "none") {
+        navEl.style.display = "block";
+    } else {
+        navEl.style.display = "none"
+    }
 }
